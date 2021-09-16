48c20a53168cde74c99cb3b3f06b2ce0f53b8622
Madhav Jha <madhavjha@gmail.com>
2021-09-15 23:15:49 -0700

    Updated with the component

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-firestore-live-view'
+import { FirestoreLiveView } from 'react-firestore-live-view'
 import 'react-firestore-live-view/dist/index.css'
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA1efZh09ZowCO9G6JhTgiqgw7snDAqCNo",
+  authDomain: "react-firebase-course-4eaf5.firebaseapp.com",
+  projectId: "react-firebase-course-4eaf5",
+  storageBucket: "react-firebase-course-4eaf5.appspot.com",
+  messagingSenderId: "429600048668",
+  appId: "1:429600048668:web:c5035dde77bd957175c1d9",
+  measurementId: "G-SQ9VRKDSLX"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <FirestoreLiveView collectionName={"cities"} db={db} />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,61 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+import { collection, query, onSnapshot } from "firebase/firestore";
+
+
+export function FirestorePanes({ documents, selected, setSelected }) {
+  if (!documents) {
+    return null;
+  }
+  return <div className={styles.test} style={{ display: "flex" }}>
+    <div style={{ display: "flex", flexDirection: "column", borderRight: '2px solid gray', padding: '10px' }}>
+      {documents.map((doc) => (
+        <div
+          key={doc.id}
+          style={{ backgroundColor: selected === doc.id ? 'yellowgreen' : '' }}
+          onClick={() => setSelected(doc.id)}>{doc.id}
+        </div>)
+      )}
+    </div>
+    <div style={{padding: '10px'}}>
+      {documents.map((doc) => (
+        <div key={doc.id} style={{ display: selected === doc.id ? '' : 'none' }} >
+          {JSON.stringify(doc.data)}
+        </div>)
+      )}
+    </div>
+  </div>
+}
+
+
+export function FirestoreLiveView({ db, collectionName }) {
+  const [selected, setSelected] = React.useState(null);
+  const [documents, setDocuments] = React.useState([]);
+
+  React.useEffect(() => {
+    const q = query(collection(db, collectionName));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      querySnapshot.docChanges().forEach((change) => {
+        if (change.type === "added") {
+          // console.log("New city: ", change.doc.id);
+          setSelected(change.doc.id);
+        }
+        if (change.type === "modified") {
+          // console.log("Modified city: ", change.doc.data());
+        }
+        if (change.type === "removed") {
+          // console.log("Removed city: ", change.doc.data());
+        }
+      });
+      const documents = [];
+      querySnapshot.forEach((doc) => {
+        documents.push({ id: doc.id, data: doc.data() });
+      });
+      setDocuments(documents);
+    })
+    return unsubscribe;
+  }, [])
+
+  return <FirestorePanes documents={documents} selected={selected} setSelected={setSelected} />
 }
